187b3b52ba3a024af8d341d65c35968a38aeb869
vtylus <143940454+VirreT@users.noreply.github.com>
2024-10-03 09:31:54 +0200

    fixed bug

--- notes-site/notes.js
+++ notes-site/notes.js
@@ -1,4 +1,5 @@
 document.querySelector('button').addEventListener('click', function(){
-    navigator.clipboard.writeText(document.getElementById("output").value);
-    alert(document.getElementById("output").value);
+    const textToCopy = document.getElementById("output").textContent;
+    navigator.clipboard.writeText(textToCopy);
+    alert(textToCopy);
 });
