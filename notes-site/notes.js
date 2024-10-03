f1b8473135f6b2fc20a8fc715a5a365a106ae7c0
vtylus <143940454+VirreT@users.noreply.github.com>
2024-10-03 09:38:26 +0200

    fixing error

--- notes-site/notes.js
+++ notes-site/notes.js
@@ -1,6 +1,5 @@
 document.querySelector('button').addEventListener('click', function(){
     const textToCopy = document.getElementById("output").textContent;
-    textToCopy.select();
     navigator.clipboard.writeText(textToCopy);
     alert("text copied");
 });
