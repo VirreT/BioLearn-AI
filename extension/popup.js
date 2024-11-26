94aa2479ff7ccbcf1d3de1e9158d6adb1d7b55a2
vtylus <143940454+VirreT@users.noreply.github.com>
2024-11-26 15:30:55 +0100

    removed text mover for debugging

--- extension/popup.js
+++ extension/popup.js
@@ -12,11 +12,6 @@ document.getElementById("extractText").addEventListener("click", () => {
     }, (results) => {
         // Display the extracted text in the popup
         console.log(results[0].result);
-
-        const encodedText = encodeURIComponent(extractText);
-
-        // Open the new window and pass the extracted text
-        window.open(`textDisplay.html?text=${encodedText}`, '_blank', 'width=600,height=400');
     });
     });
 });
