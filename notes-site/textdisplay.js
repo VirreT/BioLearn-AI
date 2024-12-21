4d5b6b89273f72e9431eeaa3466eeda5e3640106
vtylus <143940454+VirreT@users.noreply.github.com>
2024-12-21 12:51:44 +0100

    changed folder/ filename

--- notes-site/textdisplay.js
+++
@@ -1,9 +0,0 @@
-const urlParams = new URLSearchParams(window.location.search);
-        const extractedText = urlParams.get('text');
-        document.getElementById("output").innerText = extractedText || "No text found.";
-
-document.querySelector('button').addEventListener('click', function(){
-    const textToCopy = document.getElementById("output").textContent;
-    navigator.clipboard.writeText(textToCopy);
-    alert("text copied");
-});
